c84f80843a147466deb1da5497be36adff57cda3
tarun <tchinmai7@gmail.com>
2015-01-31 12:15:08 +0530

    changed logo

--- app/src/main/java/in/co/mkube/downtown/Events_activity.java
+++ app/src/main/java/in/co/mkube/downtown/Events_activity.java
@@ -8,12 +8,10 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -40,10 +38,8 @@ public class Events_activity extends ActionBarActivity {
     private String EventName = null;
     private String Desc = null;
 
-    public TextView tv[]=new TextView[10];
-    private TextView tv1;
-    private RelativeLayout rl;
-
+    public int textv[]={R.id.info_text,R.id.info_textdet,R.id.info_text1,R.id.info_textdet1,R.id.info_text2,R.id.info_textdet2,R.id.info_text3,R.id.info_textdet3,R.id.info_text4,R.id.info_textdet4};
+    public TextView tv[];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,31 +50,17 @@ public class Events_activity extends ActionBarActivity {
         EventName = getIntent().getStringExtra("EventName");
         Desc = getIntent().getStringExtra("Desc");
         Typeface font = Typeface.createFromAsset(getAssets(), "fonts/Lato-Regular.ttf");
+        tv=new TextView[10];
+        for(int i=0;i<10;i++)
+        {
+            tv[i]=(TextView)findViewById(textv[i]);
+            tv[i].setTypeface(font);
+        }
 
-        rl = (RelativeLayout) findViewById(R.id.relativelayout);
-
-        tv1 = (TextView) findViewById(R.id.textview);
-        tv1.setText("Todays's events at "+ EventName);
-
-        tv[0] = (TextView) findViewById(R.id.info_text);
-        tv[1] = (TextView) findViewById(R.id.info_textdet);
-        tv[2] = (TextView) findViewById(R.id.info_text1);
-        tv[3] = (TextView) findViewById(R.id.info_textdet1);
-        tv[4] = (TextView) findViewById(R.id.info_text2);
-        tv[5] = (TextView) findViewById(R.id.info_textdet2);
-        tv[6] = (TextView) findViewById(R.id.info_text3);
-        tv[7] = (TextView) findViewById(R.id.info_textdet3);
-        tv[8] = (TextView) findViewById(R.id.info_text4);
-        tv[9] = (TextView) findViewById(R.id.info_textdet4);
-
-        tv[1].setTypeface(font);
-        tv[2].setTypeface(font);
-
-        CardView c1 = (CardView)findViewById(R.id.card_view_events);
-        c1.setOnClickListener(new View.OnClickListener() {
+        tv[1].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Events_activity.this, GroundOverlayActivity.class);
+                Intent intent=new Intent(Events_activity.this,GroundOverlayActivity.class);
                 startActivity(intent);
 
             }
@@ -95,7 +77,6 @@ public class Events_activity extends ActionBarActivity {
         protected void onPreExecute() {
             super.onPreExecute();
             // Showing progress dialog
-            rl.setVisibility(View.INVISIBLE);
             mProgress = new ProgressDialog(Events_activity.this);
             mProgress.setMessage("Please wait...");
             mProgress.setCancelable(false);
@@ -150,18 +131,18 @@ public class Events_activity extends ActionBarActivity {
         protected void onPostExecute(Void result) {
             super.onPostExecute(result);
             // Dismiss the progress dialog
-            rl.setVisibility(View.VISIBLE);
             if (mProgress.isShowing())
                 mProgress.dismiss();
             String s1,s2,s3,s4,s5;
-            for(int i=0;i<9;i+=2) {
-                tv[i].setText(">  "+infolist.get(i/2).get(TagEventName));
-                s1 = infolist.get(i/2).get(TagDate);
-                s2 = infolist.get(i/2).get(TagFrom);
-                s3 = infolist.get(i/2).get(TagTo);
-                s4 = infolist.get(i/2).get(TagVenue);
-                s5 = "\n Date: " + s1 + "\n Time: " + s2 + " to " + s3 + "\n Venue: " + s4;
-                Log.d("asd",infolist.get(3).get(TagEventName));
+            for(int i=0;i<10;i+=2) {
+              int  j=i/2;
+                tv[i].setText(infolist.get(j).get(TagEventName));
+                s1 = infolist.get(j).get(TagDate);
+                s2 = infolist.get(j).get(TagFrom);
+                s3 = infolist.get(j).get(TagTo);
+                s4 = infolist.get(j).get(TagVenue);
+                s5 = "\n" + s1 + "\n" + s2 + " to " + s3 + "\n" + s4;
+                Log.d("asd",s5);
                 tv[i+1].setText(s5);
             }
         }
